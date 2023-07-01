b3bcdaaa182eb0d5d6af57eabf099c4f5f6b6070
Andre07-creator <aramaldes876@gmail.com>
2023-06-30 22:32:06 -0300

    version 1.1

--- index.js
+++ index.js
@@ -1,131 +1,130 @@
-let buttonAddTechnology = document.getElementById("add-technology");
+//este form serve para utilizar a parte de salvar em uma lista somente utilizando um
+//botão do tipo submit
 let form = document.getElementById("dev-cad");
-let divShow = document.getElementById("show-techologies");
-let showP = document.getElementById('showP')
-let devList = [];
+//pega o botão para adicionar tecnologias que o dev possui experiencia
+let addTechnology = document.getElementById("add-technology");
+//indice feito para que cada id das tecnologias e de um novo usuário sejam unicas
+// e para poder utilizar o foreach com o intuito de inserir na lista que o form salva
 let i = 0;
-buttonAddTechnology.addEventListener("click", function (ev) {
+//lista criada para armazenar os devs que o form salva
+let devList = [];
+//funcões que tem o intuito de poupar tempo e deixar o codigo mais limpo sem a necesidade
+//de fazer varios creates
+//////////////////////////
+function label(text, htmlfor) {
+  const createLabel = document.createElement("label");
+  createLabel.innerText = text;
+  createLabel.htmlFor = htmlfor;
+  return createLabel;
+}
+function input(id, name, type = "text", value = "", placeholder = "") {
+  const createInput = document.createElement("input");
+  createInput.id = id;
+  createInput.name = name;
+  createInput.type = type;
+  createInput.value = value;
+  createInput.placeholder = placeholder;
+  return createInput;
+}
+///////////////////////////////
+
+//adicionando evento ao botão que salva uma nova tecnologia ao dev
+addTechnology.addEventListener("click", function (ev) {
   ev.preventDefault();
-  //referenciando a lista de tecnologias
-  let stackInputs = document.getElementById("stack-inputs");
-  //criando uma nova linha para salvar a tecnologia e o tempo de experiencia
-  let newRow = document.createElement("li");
-  //indice que serve para deixar cada elemento que for adicionado único
-  let rowIndex = i;
+  //referencias ao id da ul criada no html
+  let ulRow = document.getElementById("new-technology");
+  //cria uma nova li para ser adicionada ao ul
+  let liRow = document.createElement("li");
+  //variavel auxiliar que recebe o indice de id
+  let indexRow = i;
   i++;
-  //a nova linha recebe o indice
-  newRow.id = "input-row" + rowIndex;
-  //essa classe serve para referenciar a linha lá embaixo usando o quwryselectorAll
-  newRow.className = "inputRows";
-  //label e input de inserção da tecnologia
-  let technologyName = document.createElement("label");
-  technologyName.innerText = "Tecnologia";
-  technologyName.htmlFor = "technologyName-" + rowIndex;
-  let technologyNameIput = document.createElement("input");
-  technologyNameIput.name = "technologyName";
-  technologyNameIput.id = "technologyName-" + rowIndex;
+  //dando um id proprio para cada li
+  liRow.id = "liRow-" + indexRow;
+  //classe adicionada ao li para pode utilizar o queryselectorall mais tarde
+  liRow.className = "liRow";
+  //Input do nome da tecnologia
+  let techNameLabel = label("Nome: ", "techName-" + indexRow);
+  //repare que cada elemento recebe um id unico
+  let techNameInput = input("techName-" + indexRow, "techName");
 
-  //Grupo de botões radio de escolha da experiencia
-  //Primiro botão
-
-  let experience0a2yearsLabel = document.createElement("label");
-  experience0a2yearsLabel.innerText = "0-2 years";
-  //o html for da label precisa ser igual o id da input
-  experience0a2yearsLabel.htmlFor = "expRadio" + rowIndex + ".1";
-  let experience0a2yearsInput = document.createElement("input");
-  experience0a2yearsInput.id = "expRadio" + rowIndex + ".1";
-  experience0a2yearsInput.type = "radio";
-  experience0a2yearsInput.name = "experience-" + rowIndex;
-  experience0a2yearsInput.value = "0-2 years";
-  //Segundo botão
-  let experience3a4yearsLabel = document.createElement("label");
-  experience3a4yearsLabel.innerText = " 3-4 years";
-  experience3a4yearsLabel.htmlFor = "expRadio" + rowIndex + ".2";
-  let experience3a4yearsInput = document.createElement("input");
-  experience3a4yearsInput.id = "expRadio" + rowIndex + ".2";
-  experience3a4yearsInput.type = "radio";
-  experience3a4yearsInput.name = "experience-" + rowIndex;
-  experience3a4yearsInput.value = "3-4 years";
-  //Terceito botão
-  let experience5PlusyearsLabel = document.createElement("label");
-  experience5PlusyearsLabel.innerText = " 5+ years ";
-  experience5PlusyearsLabel.htmlFor = "expRadio" + rowIndex + ".3";
-  let experience5PlusyearsInput = document.createElement("input");
-  experience3a4yearsInput.id = "expRadio" + rowIndex + ".3";
-  experience5PlusyearsInput.type = "radio";
-  experience5PlusyearsInput.name = "experience-" + rowIndex;
-  experience5PlusyearsInput.value = "5+ years";
-
-  //botão de remoção
-  let removeButton = document.createElement("button");
-  removeButton.id = "remove-button";
-  removeButton.innerText = "Excluir";
-  //função que remove o elemento filho da lista de stackInputs
-  removeButton.addEventListener("click", function () {
-    stackInputs.removeChild(newRow);
-  });
-  divShow.removeChild(showP)
-  newRow.append(
-    technologyName,
-    technologyNameIput,
-    experience0a2yearsInput,
-    experience0a2yearsLabel,
-    experience3a4yearsInput,
-    experience3a4yearsLabel,
-    experience5PlusyearsInput,
-    experience5PlusyearsLabel,
-    removeButton
+  //Grupo de botões radio para escolha da experiência
+  //Botão 1
+  let button1 = input(
+    "experience-" + indexRow + ".1",
+    "experience" + indexRow,
+    "radio",
+    "0-2 anos"
   );
-  stackInputs.appendChild(newRow);
+  let label1 = label("0-2 anos", "experience-" + indexRow + ".1");
+  //Botão 2
+  let button2 = input(
+    "experience-" + indexRow + ".2",
+    "experience" + indexRow,
+    "radio",
+    "3-4 anos"
+  );
+  let label2 = label("3-4 anos", "experience-" + indexRow + ".2");
+  //Botão 3
+  let button3 = input(
+    "experience-" + indexRow + ".3",
+    "experience" + indexRow,
+    "radio",
+    "5+ anos"
+  );
+  let label3 = label("5+ anos", "experience-" + indexRow + ".3");
+  //botão de deletar a linha
+  let deleteButton = document.createElement("button");
+  deleteButton.id = "delete" + indexRow;
+  deleteButton.innerText = "Excluir";
+  deleteButton.addEventListener("click", function () {
+    ulRow.removeChild(liRow);
+  });
+  //adicionando os elementos na li
+  liRow.append(
+    techNameLabel,
+    techNameInput,
+    button1,
+    label1,
+    button2,
+    label2,
+    button3,
+    label3,
+    deleteButton
+  );
+  //adicionando a li na ul
+  ulRow.appendChild(liRow);
 });
-
+//aqui é utilizado o form principal do tipo submit
 form.addEventListener("submit", function (ev) {
   ev.preventDefault();
-
-  let fullName = document.getElementById("full-name").value;
-  //aqui é utilizada a classe para pegar todos os elementos contidos em inputRows
-  let inputRows = document.querySelectorAll(".inputRows");
-  //é aqui onde vai ser inseridas as tecnologias coletadas pelo foreach logo abaixo
+  //array criada para a inserção das tecnologias
   let technologies = [];
-  //quando pega a referencia do elemento com queryselectorAll ela vira tipo um array, mas não é
-  //porem da pra utilizar o foreach como iteração
-  inputRows.forEach(function (row) {
+  //pega o elemento do nome completo do dev lá do html
+  let fullName = document.getElementById("full-name").value;
+  //aqui é onde a classe do liRow é utilizada através do queryselectorall que pega uma
+  //coleção de elementos com a mesma classe
+  let liRow = document.querySelectorAll(".liRow");
+  //foreach que utiliza do queryselectorall que pega um conjunto de elementos, mas não
+  //é uma lista de array
+  liRow.forEach(function (row) {
+    //pega o id e juntamente com o nome do elemento
     let techName = document.querySelector(
-      "#" + row.id + ' input[name="technologyName"]'
+      "#" + row.id + ' input[name="techName"]'
     ).value;
-    let techExp = document.querySelector(
+    //pega o id e a experiencia que está marcada 
+    let experience = document.querySelector(
       "#" + row.id + ' input[type="radio"]:checked'
     ).value;
-    technologies.push({ name: techName, exp: techExp });
+    //um push para o array de tecnologia em forma de objeto
+    technologies.push({ name: techName, experience: experience });
   });
-  const newDev = { fullName: fullName, technologies: technologies };
-  devList.push(newDev);
-  alert("Dev cadastrado com sucesso!!!");
-
-  let registeredDevs = document.createElement("h3");
-  let p = document.createElement("div");
-  registeredDevs.htmlFor = "registeredDevs";
-  registeredDevs.innerText = "Devs cadastrados";
-  let showTechologies = "";
-  devList.forEach(function (tech) {
-    showTechologies +=
-      "Nome: " +
-      tech.fullName +
-      "\n" +
-      tech.technologies
-        .map(function (t) {
-          return "Tecnologia: " + t.name + " Experiência: " + t.exp + "\n";
-        })
-        .join("") +
-      "\n";
-  });
-
-  p.innerText = showTechologies;
-  p.id='showP'
-  p.append(registeredDevs, p)
+  //um push para o array principal incluindo o nome e a lista de tecnologias que o dev
+  //domina e, forma de objeto
+  devList.push({ fullname: fullName, technologies: technologies });
+  console.log(devList);
   document.getElementById("full-name").value = "";
-  inputRows.forEach(function (row) {
+  //limpa toda a lista de nome de tecnologias e botões radio que está contida no liRow
+  liRow.forEach(function (row) {
     row.remove();
   });
-  console.log(devList);
 });
